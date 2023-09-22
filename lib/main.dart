191404fa83a02f71cf517edca2aaef84e0e707f2
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-09-22 21:34:08 +0800

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -15,29 +15,114 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  double angle = 0;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Timer.periodic(
-      const Duration(milliseconds: 16),
-      (Timer timer) => setState(() => angle += 0.02),
-    );
-  }
+  double angleX = 0;
+  double angleY = 0;
+  double angleZ = 0;
+  int majorSegmentsCount = 64;
+  int minorSegmentsCounts = 32;
 
   @override
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.black,
-          body: Center(
-            child: CustomPaint(
-              size: const Size(500, 500),
-              painter: RenderPainter(DonutRenderer(angle)),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _buildRendererPainter(),
+                _buildAngleXSlider(),
+                _buildAngleYSlider(),
+                _buildAngleZSlider(),
+                _buildMajorSegmentsCountSlider(),
+                _buildMinorSegmentsCountSlider(),
+              ],
             ),
           ),
         ),
+      );
+
+  Widget _buildRendererPainter() => CustomPaint(
+        size: const Size(500, 500),
+        painter: RenderPainter(
+          DonutRenderer(
+            angleX: angleX,
+            angleY: angleY,
+            angleZ: angleZ,
+            majorSegmentsCount: majorSegmentsCount,
+            minorSegmentsCounts: minorSegmentsCounts,
+          ),
+        ),
+      );
+
+  Widget _buildAngleXSlider() => Column(
+        children: <Widget>[
+          Text(
+            'Angle X: ${angleX.toStringAsFixed(2)}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Slider(
+            value: angleX,
+            max: pi,
+            onChanged: (double value) => setState(() => angleX = value),
+          ),
+        ],
+      );
+
+  Widget _buildAngleYSlider() => Column(
+        children: <Widget>[
+          Text(
+            'Angle Y: ${angleY.toStringAsFixed(2)}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Slider(
+            value: angleY,
+            max: pi,
+            onChanged: (double value) => setState(() => angleY = value),
+          ),
+        ],
+      );
+
+  Widget _buildAngleZSlider() => Column(
+        children: <Widget>[
+          Text(
+            'Angle Z: ${angleZ.toStringAsFixed(2)}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Slider(
+            value: angleZ,
+            max: pi,
+            onChanged: (double value) => setState(() => angleZ = value),
+          ),
+        ],
+      );
+
+  Widget _buildMajorSegmentsCountSlider() => Column(
+        children: <Widget>[
+          Text(
+            'Major Segments Count: $majorSegmentsCount',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Slider(
+            value: majorSegmentsCount.toDouble(),
+            max: 128,
+            onChanged: (double value) =>
+                setState(() => majorSegmentsCount = value.toInt()),
+          ),
+        ],
+      );
+
+  Widget _buildMinorSegmentsCountSlider() => Column(
+        children: <Widget>[
+          Text(
+            'Minor Segments Count: $minorSegmentsCounts',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Slider(
+            value: minorSegmentsCounts.toDouble(),
+            max: 64,
+            onChanged: (double value) =>
+                setState(() => minorSegmentsCounts = value.toInt()),
+          ),
+        ],
       );
 }
